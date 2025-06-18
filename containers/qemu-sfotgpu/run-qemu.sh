3bb1ea4737dc65f1ac9ee6c5b0ef48436c1162ed
roiem1etx <roie.m@1etx.com>
2025-06-18 17:37:12 +0300

    Add Win98 SoftGPU container files

---
+++ containers/qemu-sfotgpu/run-qemu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+/usr/local/bin/setup_network.sh
+
+qemu-system-i386 \
+  -m 512 \
+  -hda /vm/win98.qcow2 \
+  -netdev tap,id=net0,ifname=tap0,script=no,downscript=no \
+  -device rtl8139,netdev=net0 \
+  -vga std \
+  -soundhw sb16 \
+  -vnc :0 &
+
+websockify --web=/usr/share/novnc/ 6080 localhost:5900
